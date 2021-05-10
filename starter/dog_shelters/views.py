c6d3fc2b9a133bb4412a27cfc1543931e36c8771
Christopher Harrison <chrhar@microsoft.com>
2021-05-10 15:59:23 -0700

    Update views.py

--- starter/dog_shelters/views.py
+++ starter/dog_shelters/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from . import models
-from django.views import generic
+# TODO: Import generic views
 
 # Create your views here.
 def shelter_list(request):
